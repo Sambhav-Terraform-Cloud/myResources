308a5f53bf2852b475e2a7cfe9b6c12f9f97f549
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-03 16:12:54 +1100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "rgname" {
+  description = "resoruce group name"
+  type        = string
+}
+
+variable "rglocation" {
+  description = "resoruce group location"
+  type        = string
+}
+
