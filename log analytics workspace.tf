2618bdfde6fc1582147f7ce062b534e3abee4574
Kumar Sambhav Pandey <sambhavpandey@gmail.com>
2023-01-04 14:25:45 +1100

    Update log analytics workspace.tf

--- log analytics workspace.tf
+++ log analytics workspace.tf
@@ -3,6 +3,5 @@ resource "azurerm_log_analytics_workspace" "law" {
   name                      = "vmloganalytics"
   resource_group_name             = azurerm_resource_group.rg.name
   location                        = azurerm_resource_group.rg.location
-  sku                       = "PerGB2018"
-  internet_query_enabled    = false
+  sku                       = "PerGB2018"  
 }
